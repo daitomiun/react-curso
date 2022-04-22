e004643bca9fda80f12eaa7937762e3645b19789
Mateo Rodriguez <mateo.rodriguez.fierro@gmail.com>
2022-04-22 11:26:54 -0500

    video numero 84 del curso

---
+++ react-reducers/src/App.js
@@ -0,0 +1,12 @@
+import Contador from "./components/Contador";
+
+function App() {
+  return (
+    <div>
+      <h1>Use reducers</h1>
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
